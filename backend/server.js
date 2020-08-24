3aecdfaab90d63cf4400de23f6bc73a6e1535814
Shrikha <mahanty.shrikha@gmail.com>
2020-08-25 00:10:00 +0530

    add backend of the app

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('_middleware/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(fileUpload());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', require('./accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('_helpers/swagger'));
+
+// upload files route
+app.use('/upload', require('./uploads/files').route);
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
